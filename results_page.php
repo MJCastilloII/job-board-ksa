61cb9afb3792433a157dd5d31b4ffe72fa1b5468
MJCastilloII <58504839+MJCastilloII@users.noreply.github.com>
2019-12-11 14:45:04 +0800

    results_page.php

---
+++ results_page.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Job Board</title>
+	<!-- RESPONSIVE TAG -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" type="text/css" href="./lib/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+	<!-- Start HTML Here -->
+	<div id="main">
+		<h1>ALL JOBS</h1>
+	<div class="title">
+		<h2>Fire Officer 1</h2>
+		<h2>Bureau of Fire Protection</h2>
+		<h2>Western Visayas</h2>
+	</div>
+
+
+
+	</div>
+	<!-- END HTML Here -->
+	<!-- Jquery Plugin -->
+	<script type="text/javascript" src="./lib/jquery/jquery.min.js"></script>
+	<!-- Bootstrap Plugin -->
+	<script type="text/javascript" src="./lib/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
