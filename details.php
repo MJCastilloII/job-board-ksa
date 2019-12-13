7a386475b30e1336bf9fcfc654d1f5fd3add1370
MJCastilloII <castilloiimaryjoy@gmail.com>
2019-12-13 15:02:45 +0800

    images and pages

--- details.php
+++ details.php
@@ -1,5 +1,14 @@
 <!DOCTYPE html>
 <html>
+<style>
+	.panel {
+    margin-bottom: 10px;
+    border: 0;
+}
+	body{
+		background: linear-gradient(to left, #019bf5, #acc9f7)
+	}
+</style>
 <head>
 	<title>Job Board</title>
 	<!-- RESPONSIVE TAG -->
@@ -14,53 +23,25 @@
 		<?php
 
 			require_once("db.php");
-        	$query = "SELECT * FROM jobs WHERE id =" . $_GET['id'];
+        	$query = "SELECT * FROM jobs WHERE id =". $_GET['id'];
         	$result = mysqli_query($db,$query);
         	if(mysqli_num_rows($result) == 1){
         		$job = mysqli_fetch_assoc($result);
         	}
 		?>
-		<div class="row">
-			<div class="col-12 col-md-6">
-				<h1>Job Description</h1><br>
-				<ul>
-				<li>Candidate must possess at least High School Diploma</li>
-				<li>Required language(s):&nbsp;English, Filipino</li>
-				<li>No work experience required.</li>
-				<li>Required Skill(s): Marketing Skills</li>
-				<li>self-driven, competent and dynamic person</li>
-				<li>Result oriented &amp; with advance selling &amp; negotiation skills</li>
-				<li>Must have a Driver's Lisence at least Non-Pro Restriction 1 &amp; 2</li>
-				<li>Applicant must be residing within the area or willing to be assigned in the said area.</li>
-			</ul>
-			</div>
-			<div class="col-12 col-md-6">
-				<br><br><br>
-				Title: <b>Business Developer Representative</b><br>
-				Company Name: <b>ZESTO CORPORATION</b><br>
-				Work Location: <b>ANTIPOLO</b><br>
-				<button type="btn" class="btn btn-dark">Apply Now</button>
-			</div>
+	<div class="row">
+		<div class="col-12 col-md-6">
+			<br><br>
+			<h1>Job Description</h1>
+			<br>
+			<li><?php echo $job['description']; ?></li>
 		</div>
 
-		<div class="col-12 col-md-6">
-				<h1>Job Description</h1><br>
-				<ul>
-				<li>Candidate must possess at least High School Diploma</li>
-				<li>Required language(s):&nbsp;English, Filipino</li>
-				<li>No work experience required.</li>
-				<li>Required Skill(s): Marketing Skills</li>
-				<li>self-driven, competent and dynamic person</li>
-				<li>Result oriented &amp; with advance selling &amp; negotiation skills</li>
-				<li>Must have a Driver's Lisence at least Non-Pro Restriction 1 &amp; 2</li>
-				<li>Applicant must be residing within the area or willing to be assigned in the said area.</li>
-			</ul>
-			</div>
 			<div class="col-12 col-md-6">
 				<br><br><br>
 				Title: <b><?php echo $job['title']; ?></b><br>
-				Company Name: <b>Tech Mahindra Ltd.</b><br>
-				Work Location: <b>Eastwood City</b><br>
+				Company Name: <b><?php echo $job['company_name']; ?></b><br>
+				Work Location: <b><?php echo $job['location']; ?></b><br>
 				<button type="btn" class="btn btn-dark">Apply Now</button>
 			</div>
 		</div>
--- results_page.php
+++ results_page.php
@@ -7,16 +7,25 @@
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" type="text/css" href="./lib/bootstrap/css/bootstrap.min.css">
 	<style>
-	.panel-body{
-		padding: 5px;
+	body{
+		background: linear-gradient(to left, #019bf5, #acc9f7)
 	}
-	.h2{
-		margin-top: 0;
-		margin-bottom: 10px;
-		color: #1c3f94;
+	figure {
+	    border: thin #c0c0c0 solid;
+	    display: flex;
+	    flex-flow: column;
+	    padding: 5px;
+	    max-width: 120px;
+	    margin: auto;
+	}
+	.panel-body{
+		padding: 15px;
 	}
 	p {
 	  font-family: "Times New Roman", Times, serif;
+	}
+	.padding-bottom-60 {
+	    padding-bottom: 60px;
 	}
 	</style>
 </head>
@@ -28,39 +37,39 @@
       <div class="container">
 
         <div class="row mb-5 justify-content-center">
-          <div class="col-md-7 text-center">
-            <h2 class="section-title mb-2">Job Listed</h2>
+          <div class="col-md-4 text-center">
+            <h2 class="section-title mb-2"><strong>All Jobs</h2></strong>
           </div>
         </div>
-
+ 		
+        
         <?php 
         	require_once("db.php");
         	$query = "SELECT * FROM jobs";
         	$result = mysqli_query($db,$query);
         	while($row = mysqli_fetch_assoc($result)){
-				echo "<div class='mb-5'>
-			          <div class='row align-items-start job-item border-bottom pb-3 mb-3 pt-3'>
+				echo "<span><div class='col-mb-3'>
+			          <div class='row align-items-center job-item border-bottom pb-3 mb-3 pt-3'>
 			            <div class='col-md-2'>
 			              <a href='job-single.html'></a>
 			            </div>
-			            <div class='col-md-4'>
-			              <h2><a href='job-single.html'>".$row["title"]."</a> </h2>
-			             <strong>".$row["company_name"]."</strong>
+			            <div class='col-md-3 text-md-left'>
+			              <h2>".$row["title"]."</a> </h2>
+			             ".$row["company_name"]."
 			            </div>
 			            <div class='col-md-3 text-left'>
 			              <span class='meta'>".$row["location"]."</span>
 			            </div>
-			            <div class='col-md-3 text-md-right'>
-			              <button type='btn btn-info'><a href='details.php?id=".$row["id"]."'>See More Details</button></a>
+			            <div class='col-md-3 text-md-left'>
+			             <button type='btn' class='btn btn-warning'><a href='details.php?id=".$row["id"]."'><font color='black'>See More Details</button></a></font>
 			            </div>
 			          </div>";
         	}
         ?>
-         
 
         <div class="row pagination-wrap">
 
-          <div class="col-md-6 text-center text-md-left">
+          <div class="col-6 text-center text-md-left">
             <div class="custom-pagination ml-auto">
               <a href="#" class="prev">Previous</a>
               <div class="d-inline-block">
@@ -73,11 +82,24 @@
             </div>
           </div>
         </div>
-
-      </div>
     </section>
+    <br>
+    <div class="container-fluid bg-2 text-center"><center><strong><h2>Top Employers</strong></center></div>
+ 	<br><br>
+ 	<div class="row">
+ 		<figure><img src="./assets/img/metro-logo.jpg" width="100px" height="100px"></figure>
+ 		<figure><img src="./assets/img/startek-logo.jpg" width="100px" height="100px"></figure>
+        <figure><img src="./assets/img/sm-logo.jpg" width="100px" height="100px" ></figure>
+        <figure><img src="./assets/img/meralco-logo.jpg" width="100px" height="100px"></figure>
+        <figure><img src="./assets/img/optum-logo.jpg" width="100px" height="100px"></figure>
+        <figure><img src="./assets/img/citiglobal-logo.jpg" width="100px" height="100px"></figure>
+        <figure><img src="./assets/img/metrobank-logo.jpg" width="100px" height="100px"></figure>
 
+        </div>
+    </div>
 	</div>
+
+
 	<!-- END HTML Here -->
 	<!-- Jquery Plugin -->
 	<script type="text/javascript" src="./lib/jquery/jquery.min.js"></script>
